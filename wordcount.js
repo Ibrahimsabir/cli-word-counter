599f4594097e0f9f83f45fd528cbb21cf2934be1
Saleha Khan <161796561+Ibrahimsabir@users.noreply.github.com>
2024-04-18 10:36:01 -0700

    Add files via upload

---
+++ wordcount.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence for count the words:"
+    }
+]);
+const Words = answer.sentence.trim().split(" ");
+console.log(chalk.bold.blue(Words));
+console.log(chalk.bold.red(`Word count of your sentence is : ${Words.length}`));
